d9b216897db418647177d3193b7b2163917ad00c
Victor Hugo França <vitor@sixcreative.com.br>
2020-11-22 23:10:55 -0300

    upload image, show, index, create

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import OrphanagesControllers from "./controllers/orphanagesControllers";
+import OrphanagesControllers from "./controllers/OrphanagesControllers";
+import multer from 'multer'
+import uploadConfig from './config/upload'
+
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get('/orphanages/:id', OrphanagesControllers.show);
 routes.get('/orphanages', OrphanagesControllers.index);
-routes.post('/orphanages/create', OrphanagesControllers.create);
+routes.post('/orphanages/create', upload.array('images'), OrphanagesControllers.create);
 
 export default routes;
